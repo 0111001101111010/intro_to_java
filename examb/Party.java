87f0c9d81f9af9c65fa4ffedfdc409725942a926
stanzheng <dev.stanzheng@gmail.com>
2014-04-03 11:47:12 -0400

    size

--- examb/Party.java
+++ examb/Party.java
@@ -1,14 +1,13 @@
 import java.io.*;
 import java.util.List;
-
+import java.util.Arrays;
 import java.util.ArrayList;
-class Party{
-	int maxGuests = 5;
-    List<Integer> list = new ArrayList<Integer>();
+class Party{;
 	public static void main(String[] args) {
-
-		while (list.size()<maxGuests){
-
-		}//end of while
+	int[] guestList;
+ 	int maxGuests = 5;
+	guestList = new int[maxGuests];
+    //List<Integer> guestlist =  new ArrayList<Integer>();
+	System.out.println(guestList.length);
 	}//end of main
 }//end of class
