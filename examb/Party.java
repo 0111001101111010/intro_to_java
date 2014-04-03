bd8d9e16fd796211ae81c1deb48cd663ed4a8a89
stanzheng <dev.stanzheng@gmail.com>
2014-04-03 11:33:53 -0400

    addin arraylist

--- examb/Party.java
+++ examb/Party.java
@@ -1,5 +1,14 @@
+import java.io.*;
+import java.util.List;
+
+import java.util.ArrayList;
 class Party{
+	int maxGuests = 5;
+    List<Integer> list = new ArrayList<Integer>();
 	public static void main(String[] args) {
 
-	}
-}
+		while (list.size()<maxGuests){
+
+		}//end of while
+	}//end of main
+}//end of class
